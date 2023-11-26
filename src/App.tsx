8f6601ed2a002389a1bc0dacc49b510ec5295c48
Adreanyela Rincon <adreanyela@gmail.com>
2023-11-26 16:23:23 -0400

    inicio del curso

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="App">
+        <h1>React Typescript Template</h1>
+      </div>
+    </> 
+  )
+}
+
+export default App
